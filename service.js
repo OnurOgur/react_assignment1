e19e3c70c25ed7d9ce1c158c78fec0270b42a811
OnurOgur <80548120+OnurOgur@users.noreply.github.com>
2021-09-12 21:31:03 +0300

    first commit

---
+++ service.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+async function getData() {
+
+    const { data: users } = await (
+        axios("https://jsonplaceholder.typicode.com/users/1")
+    );
+    const { data: posts } = await (
+        axios("https://jsonplaceholder.typicode.com/posts/1")
+    );
+
+    console.log("users", users, posts);
+
+}
+
+getData();
+
+export default getData;
